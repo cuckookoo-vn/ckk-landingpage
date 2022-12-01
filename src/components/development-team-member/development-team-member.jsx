280f07a718a5d2871354130cf0f5aae71862c159
Roots-Minh <113876526+Roots-Minh@users.noreply.github.com>
2022-12-01 17:19:45 +0700

    roots: responsive 70%

--- src/components/development-team-member/development-team-member.jsx
+++ src/components/development-team-member/development-team-member.jsx
@@ -1,13 +1,10 @@
 import './development-team-member.scss'
 
 const DevelopmentTeamMember = ({image, name ,position}) =>{
-    const images = {
-        boxMember: "https://d2lonqwqrbh3kq.cloudfront.net/about-us/ckksite-about-us-team-frame.png",
-    }
+
     return(
         <div className="development-team-member">
-            <div className="development-team-member-box"
-                 style={{backgroundImage:`url(${images.boxMember})`}}>
+            <div className="development-team-member-box">
                 <img className="image-member"
                      src={image}
                      alt="member" />
