d9d3f23e6b48168d8fbe0404c4b54d1fab2763e5
jakiedoanckk <jakie.doan@cuckookoo.vn>
2022-12-08 17:25:19 +0700

    Products reponsive.

--- src/locale/language/ko.js
+++ src/locale/language/ko.js
@@ -2,144 +2,144 @@ const ko = {
   translation: {
     header: {
       menu: {
-        products: 'Products',
-        technology: 'Technology',
-        partners: 'Partners',
-        aboutUs: 'About us',
-        careers: 'Careers',
-        support: 'Support',
-        lang: 'Language',
+        products: "Products",
+        technology: "Technology",
+        partners: "Partners",
+        aboutUs: "About us",
+        careers: "Careers",
+        support: "Support",
+        lang: "Language",
       },
 
       language: {
-        english: 'English',
-        korean: 'Korean',
-        japanese: 'Japanese',
+        english: "English",
+        korean: "Korean",
+        japanese: "Japanese",
       },
     },
 
     metalish: {
       title:
-        'English Application for Student form grade 1 to grade 12 following the curriculum given by Ministry of Education.',
-      learnMore: 'Learn more',
+        "English Application for Student form grade 1 to grade 12 following the curriculum given by Ministry of Education.",
+      learnMore: "Learn more",
     },
 
     products: {
       artw: {
-        titleMain: 'Around the world',
+        titleMain: "Around the world",
         description:
-          'Around the world is a game that gives you the experience of traveling around the world.',
-        learnMore: 'Learn more',
+          "Around the world is a game that gives you the experience of traveling around the world.",
+        learnMore: "Learn more",
       },
       metalish: {
-        titleMain: 'Metalish',
+        titleMain: "Metalish",
         description:
-          'Metalish is an English learning application focusing on main English skills: listening, speaking, vocabulary, mouth shape.',
-        learnMore: 'Learn more',
+          "Metalish is an English learning application focusing on main English skills: listening, speaking, vocabulary, mouth shape.",
+        learnMore: "Learn more",
       },
     },
 
     technology: {
       health: {
-        title: 'Products in health/medical field',
+        title: "보건/의료에 상품",
       },
       alqq: {
-        title: 'Products in AI field',
+        title: "AI 필드에 상품",
       },
       bc: {
-        title: 'Blockchain development',
+        title: "블록체인 개발",
       },
       ame: {
-        title: 'Games development',
+        title: "게임 개발",
       },
       net: {
-        title: 'Network development',
+        title: "네트워크 개발",
       },
 
       technologyUse: {
-        title1: 'TECHNOLOGY',
-        title2: 'we are',
-        title3: 'USING for our',
-        title4: 'VALUED PARTNERS',
+        title1: "TECHNOLOGY",
+        title2: "we are",
+        title3: "USING for our",
+        title4: "VALUED PARTNERS",
       },
     },
 
     aboutUs: {
       cuckookooTeam: {
-        titleMain: 'About us',
+        titleMain: "About us",
         description:
           "Cuckookoo's orientation is to develop community application products in the " +
-          'fields of: health, education, games, fashion in the direction of metaverse.',
+          "fields of: health, education, games, fashion in the direction of metaverse.",
       },
     },
 
     partners: {
       ourPartners: {
-        titleMain1: 'OUR',
-        titleMain2: 'PARTNERS',
+        titleMain1: "OUR",
+        titleMain2: "PARTNERS",
       },
 
       review: {
-        titleMain: 'What partners say about Cuckookoo?',
+        titleMain: "What partners say about Cuckookoo?",
       },
     },
 
     careers: {
-      title1: 'WE ARE',
+      title1: "WE ARE",
 
-      title2: 'LOOKING FOR',
+      title2: "LOOKING FOR",
 
       job1: {
-        title: '1. Job’s title 1',
-        time: '29/11/2022',
+        title: "1. Job’s title 1",
+        time: "29/11/2022",
       },
 
       job2: {
-        title: '2. Job’s title 2',
-        time: '29/12/2022',
+        title: "2. Job’s title 2",
+        time: "29/12/2022",
       },
 
       job3: {
-        title: '3. Job’s title 3',
-        time: '20/11/2022',
+        title: "3. Job’s title 3",
+        time: "20/11/2022",
       },
 
-      buttonApply: 'Apply',
+      buttonApply: "Apply",
     },
 
     support: {
-      titleMain: 'SUPPORT',
+      titleMain: "SUPPORT",
 
       description:
-        'Leave us a message by filling out our form below and we will get back to you very soon!',
+        "Leave us a message by filling out our form below and we will get back to you very soon!",
 
       email: {
-        titleEmail: 'Your email address',
-        required: 'required',
+        titleEmail: "Your email address",
+        required: "required",
       },
 
       file: {
-        titleFile: 'Attched file (Max: 25MB)',
-        required: 'required',
-        maxSize: 'maxsize',
+        titleFile: "Attched file (Max: 25MB)",
+        required: "required",
+        maxSize: "maxsize",
       },
 
       message: {
-        titleMessage: 'Your message',
-        required: 'required',
-        maxLength: 'max 350',
+        titleMessage: "Your message",
+        required: "required",
+        maxLength: "max 350",
       },
 
-      buttonTitle: 'Submit',
+      buttonTitle: "Submit",
     },
 
     footer: {
-      titleAddress: 'Address',
+      titleAddress: "Address",
       address:
-        'No. 82 Street 6, CityLand Park Hills Residential Area, Ward 10, Go Vap District, Ho Chi Minh City',
-      contactUs: 'Contact us',
-      email: 'Email',
-      phoneNumber: 'Phone number',
+        "No. 82 Street 6, CityLand Park Hills Residential Area, Ward 10, Go Vap District, Ho Chi Minh City",
+      contactUs: "Contact us",
+      email: "Email",
+      phoneNumber: "Phone number",
     },
   },
 };
